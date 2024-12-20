625d4a10815e1992053b12dabd4d9cf13fd8212c
lanhunn <22341083@zju.edu.cn>
2024-12-20 15:34:40 +0800

    fix the problem in sync_reset_clk_gating

--- rtl_auto_connect/sync_reset_clk_gate.v
+++ rtl_auto_connect/sync_reset_clk_gate.v
@@ -20,11 +20,10 @@ module sync_reset_clk_gate #(
   wire [CNT_WD-1:0] cur_counter;
 
   wire              cur_clk_en;
-  wire              nxt_clk_en;
 
 
   assign clk_en       = arcg_on ? cur_clk_en : 1'b1;
-  assign counter_wren = (cur_counter < DELAY);
+  assign counter_wren = (cur_counter != DELAY);
   assign nxt_counter  = cur_counter + {{(CNT_WD - 1) {1'b0}}, 1'b1};
 
   BB_dfflr #(
@@ -38,19 +37,6 @@ module sync_reset_clk_gate #(
       .dout (cur_counter)
   );
 
-
-  assign nxt_clk_en = counter_wren;
-
-  BB_dffr #(
-      .DW     (1),
-      .RST_VAL(0)
-  ) u_BB_clk_en_dfflr (
-      .clk  (i_clk),
-      .rst_n(src_rst_n),
-      .din  (nxt_clk_en),
-      .dout (cur_clk_en)
-  );
-
-
+  assign cur_clk_en = (!counter_wren);
 
 endmodule  //moduleName
